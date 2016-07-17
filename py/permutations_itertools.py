8d1fc001f2672a80bba5a6cb69ebd1723abe2e69
vanryan <zvonryan@gmail.com>
2016-07-17 14:54:35 -0700

    [py] Add permutation using itertools

---
+++ py/permutations_itertools.py
@@ -0,0 +1,23 @@
+# Input: A single line containing the space separated string and the integer value 
+# Print the permutations of the string on separate lines. 
+from itertools import permutations
+s, k = raw_input().split()
+k = int(k)
+s = permutations(sorted(list(s)), k)
+for f in s:
+    print ''.join(f)
+
+'''
+>>> from itertools import permutations
+>>> print permutations(['1','2','3'])
+<itertools.permutations object at 0x02A45210>
+>>> 
+>>> print list(permutations(['1','2','3']))
+[('1', '2', '3'), ('1', '3', '2'), ('2', '1', '3'), ('2', '3', '1'), ('3', '1', '2'), ('3', '2', '1')]
+>>> 
+>>> print list(permutations(['1','2','3'],2))
+[('1', '2'), ('1', '3'), ('2', '1'), ('2', '3'), ('3', '1'), ('3', '2')]
+>>>
+>>> print list(permutations('abc',3))
+[('a', 'b', 'c'), ('a', 'c', 'b'), ('b', 'a', 'c'), ('b', 'c', 'a'), ('c', 'a', 'b'), ('c', 'b', 'a')]
+'''
