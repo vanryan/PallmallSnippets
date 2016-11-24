60941815404dc0889e61f4a6e9920bf4e3c63dcf
vanryan <zvonryan@gmail.com>
2016-11-24 14:03:51 -0500

    Add big integer pool "memory leak" test

---
+++ py_tests/test_py_bigint_pool_.py
@@ -0,0 +1,15 @@
+'''
+1 GB memory can hold the int objects from 0 to 89478486, if a PyIntObject has a size of 12 bytes.
+
+In int_dealloc, no memory will be given back to the system heap until the python process is over, which is kind of like memory leak.
+
+On Macbook Pro mid 2015, 2.5 Ghz i7, 16 GB 1600 MHz DDR3, this process takes around 2.72GB memory
+'''
+
+
+l = list()
+
+for i in range(0, 89478486):
+    l.append(i)
+    if len(l) % 10000 == 0:
+        l[:] = []
