bb0cd72d2d5b288fb4a4b19e47d8dc8a67fd28d0
vanryan <zvonryan@gmail.com>
2016-11-27 17:08:20 -0500

    minor change

--- py_tests/test_py_bigint_pool_.py
+++ py_tests/test_py_bigint_pool_.py
@@ -3,13 +3,23 @@
 
 In int_dealloc, no memory will be given back to the system heap until the python process is over, which is kind of like memory leak.
 
-Run in Python 2.7.11, on Macbook Pro mid 2015, 2.5 Ghz i7, 16 GB 1600 MHz DDR3, this process takes around 2.72GB memory
+Run in Python 2.7.11, on Macbook Pro mid 2015, 2.5 Ghz i7, 16 GB 1600 MHz DDR3, num1 in this process takes around 2.72GB memory
 '''
-
 
 l = list()
 
-for i in range(0, 89478486):
-    l.append(i)
-    if len(l) % 10000 == 0:
-        l[:] = []
+num1 = 89478486
+num2 = 178956971
+
+def cal():
+	for i in range(0, num1):
+	    l.append(i)
+	    if len(l) % 10000000 == 0:
+	        l[:] = []
+
+#dis.dis(cal)
+
+'''
+for i in range(0, 178956971):
+    pass
+'''
